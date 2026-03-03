0eb36b8d873d1d5a34f88d3b185ddab8b731e1ce
Lakshmi-CSE24 <lakshmisaraswathi2006@gmail.com>
2026-03-03 14:33:26 +0530

    UC2: Print a Hardcoded Palindrome Result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,32 +3,26 @@
  * MAIN CLASS – PalindromeCheckerApp
  * ============================================================
  *
- * Use Case 1: Application Entry & Welcome Message
+ * Use Case 2: Hardcoded Palindrome Validation
  *
  * Description:
- * This class represents the entry point of the
- * Palindrome Checker Management System.
+ * This class demonstrates basic palindrome validation
+ * using a hardcoded string value.
  *
  * At this stage, the application:
- * - Starts execution from the main() method
- * - Displays a welcome message
- * - Shows application version
- *
- * No palindrome logic is implemented yet.
- *
- * The goal is to establish a clear startup flow.
+ * - Stores a predefined string
+ * - Compares characters from both ends
+ * - Determines whether the string is a palindrome
+ * - Displays the result on the console
  *
  * @author Developer
- * @version 1.0
+ * @version 2.0
  */
 
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
+     * Application entry point for UC2.
      *
      * @param args Command-line arguments
      */
@@ -39,8 +33,24 @@ public class PalindromeCheckerApp {
         System.out.println("      PALINDROME CHECKER APP");
         System.out.println("=======================================");
 
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version : 1.0");
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+
+                isPalindrome = false;
+                break;
+
+            }
+        }
+
+        System.out.println("Input String : " + input);
+        System.out.println("Is Palindrome : " + isPalindrome);
 
     }
 }
